047b3075c1bb5c4f0c18962afce9d2b42af4154c
luskoeqk <lusy2017@gmail.com>
2023-03-24 16:46:10 +0200

    head

--- src/components/LayoutComponents/Header.tsx
+++ src/components/LayoutComponents/Header.tsx
@@ -7,22 +7,18 @@ import '@/styles/layouts/header.scss';
 export const Header = () => {
     return (
         <header className="header">
-            
+
             <div className="top-panel">
                 <div className="container">
-                    <div className="header-left">
-                        <ul className="header-cont">
-                            <li><i className="fa fa-phone" aria-hidden="true"></i><a href="tel:+359866405663">+359866405663</a></li>
-                            <li><i className="fa fa-clock-o" aria-hidden="true"></i>Понеделник и сряда - 20:30, стадион "Академик" (4-ти км)</li>
-                        </ul>
-                    </div>
-                    <div className="header-right">
-                        <ul className="social-list">
-                            <li><a target="_blank" href="https://www.facebook.com/afcsofiaknights"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
-                            <li><a target="_blank" href="https://www.instagram.com/sofiaknightsafc"><i className="fa fa-instagram" aria-hidden="true"></i></a></li>
-                            <li><a target="_blank" href="https://www.youtube.com/@sofiaknightsafc7904"><i className="fa fa-youtube" aria-hidden="true"></i></a></li>
-                        </ul>
-                    </div>
+                    <ul className="header-cont">
+                        <li><i className="fa fa-phone" aria-hidden="true"></i><a href="tel:+359866405663">+359866405663</a></li>
+                        <li><i className="fa fa-clock-o" aria-hidden="true"></i>Понеделник и сряда - 20:30, стадион "Академик" (4-ти км)</li>
+                    </ul>
+                    {/* <ul className="social-list"> */}
+                        {/* <li><a target="_blank" href="https://www.facebook.com/afcsofiaknights"><i className="fa fa-facebook" aria-hidden="true"></i></a></li> */}
+                        {/* <li><a target="_blank" href="https://www.instagram.com/sofiaknightsafc"><i className="fa fa-instagram" aria-hidden="true"></i></a></li> */}
+                        {/* <li><a target="_blank" href="https://www.youtube.com/@sofiaknightsafc7904"><i className="fa fa-youtube" aria-hidden="true"></i></a></li> */}
+                    {/* </ul> */}
                 </div>
             </div>
 
@@ -35,9 +31,13 @@ export const Header = () => {
                     <nav className="nav-menu">
                         <ul className="nav-list">
                             <li className="menu-active"><a href="index.html">Начало</a></li>
+                            |
                             <li><a href="history.html">История</a></li>
+                            |
                             <li><a href="practices.html">Тренировки</a></li>
+                            |
                             <li><a href="coaches.html">Треньори</a></li>
+                            |
                             <li><a href="contacts.html">Контакти</a></li>
                         </ul>
                     </nav>
